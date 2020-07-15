bfbb57f805933f43fa5f3897c06aefad48b0dd8d
Alwin <alwinjohn231@gmail.com>
2020-07-15 16:01:04 +0530

    frog jump program added via upload

---
+++ Week-1/Day-5/FrogJump.py
@@ -0,0 +1,8 @@
+def kangaroo(x1, v1, x2, v2):
+    return "YES" if (v1 > v2) and not (x2 - x1) % (v2 - v1) else "NO"
+
+t = int(input())
+for _ in range(0,t):
+    x1,v1,x2,v2 = map(int,input().split())
+    result = kangaroo(x1, v1, x2, v2)
+    print(result)
